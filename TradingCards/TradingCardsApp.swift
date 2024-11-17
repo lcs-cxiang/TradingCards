3b70bdbdec017ab5df9b0eb8f7098f20092fb229
Kyle Xiang <cxiang@lcs.on.ca>
2024-11-17 02:49:04 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Chujun Xiang on 2024-11-17.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
